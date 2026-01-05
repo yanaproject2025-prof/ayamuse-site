c6c0b82179579e6f7254b895828e6fda8ec3a316
yanaproject2025-prof <yanaproject2025@gmail.com>
2026-01-06 00:16:17 +0300

    Update load-news.js

--- js/load-news.js
+++ js/load-news.js
@@ -9,7 +9,7 @@ async function loadNews() {
     container.innerHTML = items.map(item => `
       <article class="card">
         <h3 class="card__title">${item.title}</h3>
-        <div class="card-date">${item.date}</div>
+        <p class="letter__date">${item.date}</p>
         <p class="card__text">${item.excerpt}</p>
         <a href="${item.link}" target="_blank" rel="noopener" class="card__link">
           ${item.link_text}

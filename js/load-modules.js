511b8fb84a06573a926eed40dab9b1c403fc2bb7
yanaproject2025-prof <yanaproject2025@gmail.com>
2026-01-06 17:55:46 +0300

    Update load-modules.js

--- js/load-modules.js
+++ js/load-modules.js
@@ -52,9 +52,44 @@ async function loadNewsModules() {
   } catch (e) {
     console.error('Error loading news:', e);
   }
+
+  // ----------------------------------------------------
+// Load data for FUTURE section
+// ----------------------------------------------------
+
+async function loadFutureModules() {
+  try {
+    const data = await fetch('/modules/future/future.json').then(r => r.json());
+    const items = data.items.slice(0, 2);
+
+    const container = document.querySelector('#future .grid--2');
+    if (!container) return;
+
+    container.innerHTML = items.map(item => `
+      <article class="card card--future">
+
+        <img src="${item.image}" alt="${item.title}" class="card__image">
+
+        <h3 class="card__title">${item.title}</h3>
+
+        <p class="card_date">${item.date}</p>
+
+        <p class="card__text">${item.excerpt}</p>
+
+        <span class="card__soon">${item.coming}</span>
+
+      </article>
+    `).join('');
+
+  } catch (e) {
+    console.error('Error loading future section:', e);
+  }
 }
 
+document.addEventListener('DOMContentLoaded', loadFutureModules);
 
+  
+}
 
 // ----------------------------------------------------
 // INIT MODULES
